60c704bec098e732eb1b51d7a914dbc528a879df
SSS <stendhal.syndrome.studio@gmail.com>
2024-10-06 21:29:13 +0100

    minor changes

--- bot/main.py
+++ bot/main.py
@@ -16,7 +16,8 @@ async def on_startup(dispatcher: Dispatcher):
     log.info('bot started...')
     await sql_start()
 
-    scheduler.add_job(check_every_day, 'interval', minutes=1, args=(dispatcher,))
+    # scheduler.add_job(check_every_day, 'interval', minutes=1, args=(dispatcher,))
+    scheduler.add_job(check_every_day, 'interval', days=30, args=(dispatcher,))
     scheduler.start()
 
     dispatcher.register_message_handler(request_start_handler)
---
+++ deploy/deploy_raspberry.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt install docker.io
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+curl -sSL https://get.docker.com | sh
+
+sudo curl -L https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-`uname -s`-`uname -m` > docker-compose
+sudo mv docker-compose /usr/bin/
+sudo chown root: /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
